e94c2d9016d0d81db34a54f6c6c4619114d97779
DG <p.kittichet@gmail.com>
2022-07-31 23:36:12 +0700

    add new release book

--- assets/js/dg_main.js
+++ assets/js/dg_main.js
@@ -2,3 +2,19 @@ document.querySelector("#backtotop").onclick = function() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 };  
+
+let items = document.querySelectorAll('.carousel-mul .carousel-item')
+
+		items.forEach((el) => {
+			const minPerSlide = 4
+			let next = el.nextElementSibling
+			for (var i=1; i<minPerSlide; i++) {
+				if (!next) {
+            // wrap carousel by using first child
+            next = items[0]
+        }
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
