822577adf88cfc48abbe6823d94859c848d8c00e
DG <p.kittichet@gmail.com>
2022-08-02 22:03:27 +0700

    update complete index next register

--- assets/js/dg_main.js
+++ assets/js/dg_main.js
@@ -39,9 +39,29 @@ $(document).ready(function(){
             }
         }
     });
+    $('.owl-carousel-promotion').owlCarousel({
+        loop:true,
+        margin:50,
+        nav:false,
+        dots:false,
+        autoWidth:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            1000:{
+                items:1,
+            },
+            1600:{
+                items:2,
+            },
+
+        }
+    });
     $('.owl-carousel-news').owlCarousel({
         loop:true,
-        margin:30,
+        margin:60,
         nav:false,
         dots:false,
         autoWidth:false,
